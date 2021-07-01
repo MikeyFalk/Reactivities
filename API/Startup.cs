c380917c1ea8742f8b6f935de134c6d71fecfe4f
Mike Falk <falk478@yahoo.com>
2021-06-30 21:20:55 -0700

    Migrated database and seeded data

--- API/Startup.cs
+++ API/Startup.cs
@@ -39,7 +39,7 @@ namespace API
             services.AddDbContext<DataContext>(opt =>
             {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            })
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
